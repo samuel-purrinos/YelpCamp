1e613cdbce34578fd24e651be703067d27ec8288
Samuel Purriños <samuel.purrinos@outlook.com>
2023-03-23 18:21:34 +0100

    Implemented reviews feature in the application

--- schemas.js
+++ schemas.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+
 module.exports.campgroundSchema = Joi.object({
     campground : Joi.object({
         title : Joi.string().required(),
@@ -6,5 +7,12 @@ module.exports.campgroundSchema = Joi.object({
         image : Joi.string().required(),
         location : Joi.string().required(),
         description : Joi.string().required()
+    }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+    review : Joi.object({
+        rating : Joi.number().required().max(5).min(0),
+        body : Joi.string().required()
     }).required()
 });
