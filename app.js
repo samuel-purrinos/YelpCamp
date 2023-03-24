10cf73fba498bd46176fe8d7d20bfe2fdcc3f8a6
Samuel Purriños <samuel.purrinos@outlook.com>
2023-03-24 15:13:30 +0100

    Implemented authentication
    Refactor and implemented authorization for reviews and campgrounds
    
    Refactor and implemented authorization for reviews and campgrounds
    
    Implemented Authorization

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const methodOverride = require('method-override');
 const dbString = require('./dbString');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 mongoose.connect(dbString.dbString);
 
@@ -39,7 +43,15 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
@@ -47,9 +59,9 @@ app.use((req,res,next)=>{
 
 
 
-
-app.use('/campgrounds',campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/',userRoutes);
+app.use('/campgrounds',campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewsRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20)+10;
         const camp = new Campground({
+            author : '641d9a21b21d6b440c386439',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
